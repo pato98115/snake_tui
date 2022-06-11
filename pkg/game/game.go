ca10205daadaf1a3afbc0588be168d5f074cf6cb
pato98115 <pato98115@gmail.com>
2022-06-10 23:33:35 -0300

    first draft of snake+board+fruits state logic

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -1,13 +1,14 @@
 package game
 
 import (
-	"fmt"
-
 	deque "github.com/pato98115/snake_tui/pkg/deque"
 )
 
 // Snake direction in board
-type direction byte
+type Direction byte
+
+// board cell type
+type CellType byte
 
 // Cell for composing snake's body
 type cell struct {
@@ -28,18 +29,20 @@ type food struct {
 }
 
 // Matrix like board
-type board struct {
-	rows uint
-	cols uint
+type Board struct {
+	Rows  uint
+	Cols  uint
+	cells []CellType
 }
 
 // Game
 type Game struct {
-	snake     snake
-	snake_dir direction
-	food      []food
-	board     board
-	points    uint
+	snake    snake
+	snakeDir Direction
+	food     *deque.Deque
+	Board    Board
+	Points   uint
+	running  bool
 }
 
 const (
@@ -48,10 +51,17 @@ const (
 
 	snakeStartSize uint = 3
 
-	dirUp direction = iota
-	dirDown
-	dirLeft
-	dirRight
+	Up Direction = iota
+	Right
+	Down
+	Left
+
+	BackgroundCell CellType = iota
+	FruitCell1
+	FruitCell2
+	FruitCell3
+	SnakeBodyCell
+	SnakeHeadCell
 )
 
 // Compare two cells
@@ -74,27 +84,99 @@ func (s *snake) getHead() *cell {
 	return v.(*cell)
 }
 
-func (s *snake) eat(f food) error {
-	if !s.getHead().equals(f.cell) {
-		return fmt.Errorf("ERROR: Snake can't reach that")
-	}
+func (s *snake) eat(f *food) error {
 	s.food += f.quantity
 	return nil
 }
 
-func (g *Game) changeDir(d direction) {
+func fruitType(f *food) CellType {
+	switch f.quantity {
+	case 1:
+		return FruitCell1
+	case 2:
+		return FruitCell2
+	default:
+		return FruitCell3
+	}
+}
+
+func (g *Game) changeDir(d Direction) {
 	// only change direction when it's a valid one
 	// depending on current direction
 	switch d {
-	case dirUp, dirDown:
-		switch g.snake_dir {
-		case dirLeft, dirRight:
-			g.snake_dir = d
+	case Up, Down:
+		switch g.snakeDir {
+		case Left, Right:
+			g.snakeDir = d
 		}
-	case dirLeft, dirRight:
-		switch g.snake_dir {
-		case dirUp, dirDown:
-			g.snake_dir = d
+	case Left, Right:
+		switch g.snakeDir {
+		case Up, Down:
+			g.snakeDir = d
 		}
 	}
+}
+
+func (g *Game) step() bool {
+	// gen next cell for snake
+	nextCell := cell{}
+	snakeHead := g.snake.getHead()
+	switch g.snakeDir {
+	case Up:
+		nextCell.x = snakeHead.x
+		nextCell.y = snakeHead.y + 1
+	case Right:
+		nextCell.x = snakeHead.x + 1
+		nextCell.y = snakeHead.y
+	case Down:
+		nextCell.x = snakeHead.x
+		nextCell.y = snakeHead.y - 1
+	case Left:
+		nextCell.x = snakeHead.x - 1
+		nextCell.y = snakeHead.y
+	}
+
+	// using uint 0 - 1 = max uint for easier check
+	if nextCell.x > g.Board.Cols || nextCell.y > g.Board.Rows {
+		g.running = false
+		return g.running
+	}
+
+	for e := g.food.Front(); e != nil; e = e.Next() {
+		f := e.Value.(*food)
+		if nextCell.equals(f.cell) {
+			g.snake.eat(f)
+			g.Points += f.quantity
+			g.food.Remove(e)
+			break
+		}
+	}
+
+	g.snake.move(&nextCell)
+
+	return true
+}
+
+func (g *Game) buildBoard() []CellType {
+	// set background
+	for i := uint(0); i < g.Board.Rows; i++ {
+		for j := uint(0); j < g.Board.Cols; j++ {
+			g.Board.cells[i+j] = BackgroundCell
+		}
+	}
+	// set fruits
+	for e := g.food.Front(); e != nil; e = e.Next() {
+		f := e.Value.(*food)
+		g.Board.cells[f.cell.x+f.cell.y] = fruitType(f)
+	}
+	// set snake head
+	h := g.snake.getHead()
+	g.Board.cells[h.x+h.y] = SnakeHeadCell
+
+	for e := g.snake.body.Front().Next(); e != nil; e = e.Next() {
+		c := e.Value.(*cell)
+		g.Board.cells[c.x+c.y] = SnakeBodyCell
+	}
+
+	return g.Board.cells
 }
